090269ecf9dcf31a3fad641c70065d2304063ea4
Ivan <ivan_90.90@mail.ru>
2017-11-23 19:22:13 +0300

    Imps

--- client/v1/feeds/user-news.js
+++ client/v1/feeds/user-news.js
@@ -2,24 +2,23 @@ var Request = require('../request');
 var Helpers = require('../../../helpers');
 
 function UserNewsFeed(session, limit) {
-    this.session = session;
     this.timeout = 10 * 60 * 1000; // 10 minutes
     this.limit = limit;
+    FeedBase.apply(this, arguments);
 }
+
+util.inherits(UserNewsFeed, FeedBase);
 
 UserNewsFeed.prototype.get = function () {
     var that = this;
-    return this.session.getAccountId()
-        .then(function(id) {
-            var rankToken = Helpers.buildRankToken(id);
-            return new Request(that.session)
-                .setMethod('GET')
-                .setResource('news', {
-                    maxId: that.getCursor(),
-                    rankToken: rankToken
-                })
-                .send()
-        });
+    var rankToken = Helpers.buildRankToken(id);
+    return new Request(that.session)
+        .setMethod('GET')
+        .setResource('news', {
+            maxId: that.getCursor(),
+            rankToken: rankToken
+        })
+        .send()
 };
 
 module.exports = UserNewsFeed;
