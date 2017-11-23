69da5e0663325bba9c1d0d9ab107dcdb330ec4e9
Ivan <ivan_90.90@mail.ru>
2017-11-23 19:39:05 +0300

    i

--- client/v1/feeds/user-news.js
+++ client/v1/feeds/user-news.js
@@ -3,7 +3,6 @@ var util = require('util');
 var FeedBase = require('./feed-base');
 
 function NewsFeed(session, limit) {
-    console.log(`Session: ${!!session}`);
     this.limit = parseInt(limit) || null;
     FeedBase.apply(this, arguments);
 }
@@ -15,7 +14,6 @@ var Helpers = require('../../../helpers');
 
 
 NewsFeed.prototype.get = function () {
-    console.log(this);
     var that = this;
     return this.session.getAccountId()
         .then(function(id) {
